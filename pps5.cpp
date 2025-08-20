27cea2384d071f242e754fa0011dd7b40b3606cf
MRINAL JHA <mrinaljha39@gmail.com>
2025-08-20 12:24:35 +0530

    Add files via upload

---
+++ pps5.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{printf("enter 2 numbers A and B\n");
+    int a;
+    int b;
+scanf("%d",& a);
+scanf("%d",& b);
+
+
+printf("A is: %d\n",b);
+printf("B is : %d",a);
+
+}
